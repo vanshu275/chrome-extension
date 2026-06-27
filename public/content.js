06a907c1c929dd5decd69e317396678809e35f80
Vansh Upadhyay <upadhyayvansh92@gmail.com>
2026-06-27 16:58:56 +0530

    version-1 failed and now another way for transcript

--- public/content.js
+++ public/content.js
@@ -1,5 +1,9 @@
+const videoId = new URL(window.location.href).searchParams.get("v");
+
 chrome.storage.local.set({
   title: document.title,
+  videoId,
 });
 
 console.log("saved");
+console.log(document.body.innerText.slice(0, 1000));
--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [title, setTitle] = useState("");
+  const [videoId, setVideoId] = useState("");
 
-  useEffect(() => {
-  chrome.storage.local.get(["title"], (result) => {
-  console.log(result);
-  setTitle(result.title);
-});
+useEffect(() => {
+  chrome.storage.local.get(
+    ["title", "videoId"],
+    (result) => {
+      setTitle(result.title);
+      setVideoId(result.videoId);
+    }
+  );
 }, []);
 
-  return <h2>{title}</h2>;
+  return(
+    <div>
+      <h2>{videoId}</h2>
+     <h2>{title}</h2>;
+  </div>
+  )
 }
 
 export default App;
